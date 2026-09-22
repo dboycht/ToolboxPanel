b66dd6f50754e8b8be697d52b55e64b3bcad322f
D BOY <69496137+dboycht@users.noreply.github.com>
2026-09-22 08:38:48 +0800

    2.0.6 release materials: changelog body + publish script (tag/release NOT created yet - waiting for user sign-off)

--- pack/publish-release.ps1
+++ pack/publish-release.ps1
@@ -1,4 +1,4 @@
-# publish-release.ps1 -- create the GitHub Release for ToolboxPanel 2.0.5
+# publish-release.ps1 -- create the GitHub Release for ToolboxPanel 2.0.6
 # ASCII-only script (PS 5.1 reads BOM-less files as ANSI; Chinese would break quoting).
 # Rule: release NAME = version number, release BODY = changelog (UTF-8 read from a file).
 # Usage: powershell -NoProfile -File pack\publish-release.ps1
@@ -7,9 +7,9 @@ $log = New-Object System.Collections.Generic.List[string]
 function Log($m) { $log.Add([string]$m) }
 
 $repo = 'dboycht/ToolboxPanel'
-$tag = '2.0.5'
-$zip = 'D:\code\github_repository\ToolboxPanel\dist\ToolboxPanel-2.0.5-win-x64.zip'
-$bodyFile = 'D:\code\github_repository\ToolboxPanel\pack\release-body-2.0.5.md'
+$tag = '2.0.6'
+$zip = 'D:\code\github_repository\ToolboxPanel\dist\ToolboxPanel-2.0.6-win-x64.zip'
+$bodyFile = 'D:\code\github_repository\ToolboxPanel\pack\release-body-2.0.6.md'
 $out = 'D:\code\github_repository\ToolboxPanel\pack\release-result.txt'
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
